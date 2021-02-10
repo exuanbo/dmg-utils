1c08ff235a6b068f78458dfa4febc375b47ed62a
exuanbo <exuanbo@protonmail.com>
2021-02-10 17:50:51 +0000

    build: update npm script, rollup conf and tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,30 @@
-const typescript = require('@rollup/plugin-typescript')
-const pkg = require('./package.json')
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import pkg from './package.json'
 
-module.exports = {
-  input: 'src/index.ts',
-  plugins: [typescript()],
-  external: ['util', 'child_process', 'fs-extra'],
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
+export default [
+  {
+    external: ['util', 'child_process', ...Object.keys(pkg.dependencies)],
+    input: 'src/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ]
+  },
+  {
+    external: ['child_process'],
+    input: '.cache/index.d.ts',
+    plugins: [dts()],
+    output: {
+      file: pkg.types,
       format: 'es'
     }
-  ]
-}
+  }
+]
